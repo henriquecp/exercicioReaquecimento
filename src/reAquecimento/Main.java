668af56bf93de7fbe11678dd76427453c5802dd7
09201802 <09201802@l1847011.portoalegre.pucrsnet.br>
2016-08-11 22:37:06 -0300

    Changes in main, generating caixas and clients

--- src/reAquecimento/Main.java
+++ src/reAquecimento/Main.java
@@ -1,7 +1,23 @@
 package reAquecimento;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
 	
+	public static void main(String args[]){
+	ArrayList<Caixa> caixas = new ArrayList<>();
+	ArrayList<Client> clientes = new ArrayList<>();
+	Random r = new Random();
 	
+		for(int i = 0; i < 10; i++){
+			caixas.add(new Caixa());
+		}
+		
+		for(int i = 0; i < 10; i++){
+			clientes.add(new Client("Joao" + i, r.nextInt(11) + 60));
+		}
+		
+	}
 	
 }
